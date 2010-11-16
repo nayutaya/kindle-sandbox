cfdc9f76248f4d7de544c56c006983cb7cb3efe9
Yuya Kato <yuyakato@gmail.com>
2010-11-16 12:14:11 +0900

    コメントの処理を追加

--- slashdotjp.rb
+++ slashdotjp.rb
@@ -97,26 +97,45 @@ p({
 
 
 puts "---"
+
 doc.xpath("//*[@id='commentlisting']//text()").
   select { |node| node.to_s.strip.empty? }.
   each   { |node| node.remove }
 
+doc.xpath("//*[@id='roothiddens']").remove
+doc.xpath("//*[@id='commentlisting']//div[@class='commentstatus']").remove
+doc.xpath("//*[@id='commentlisting']//div[@class='commentSub']").remove
+doc.xpath("//*[@id='commentlisting']//a[@class='user_journal_display']").each { |node| node.parent.remove }
+
 doc.xpath("//*[@id='commentlisting']//h4/a").each { |node|
-  text = node.text
-  text.strip!
+  text = node.text.strip
   node.replace(Nokogiri::XML::Text.new(text, doc))
 }
 
 doc.xpath("//*[@id='commentlisting']//h4/span").each { |node|
-  text = node.text
-  text.strip!
+  text = node.text.strip
   elem = Nokogiri::XML::Element.new("span", doc)
   elem.add_child(Nokogiri::XML::Text.new(text, doc))
   node.replace(elem)
 }
 
-doc.xpath("//*[@id='commentlisting']//div[@class='commentstatus']").remove
-doc.xpath("//*[@id='commentlisting']//div[@class='commentSub']").remove
+doc.xpath("//*[@id='commentlisting']//h4").each { |node|
+  node.children.each { |child|
+    node.parent.add_child(child)
+  }
+  node.remove
+}
+
+doc.xpath("//*[@id='commentlisting']//div[@class='details']//a").each { |node|
+  text = node.text.strip
+  node.replace(Nokogiri::XML::Text.new(text, doc))
+}
+
+
+doc.xpath("//*[@id='commentlisting']//li/b/a").each { |node|
+  text = node.text.strip
+  node.replace(Nokogiri::XML::Text.new(text, doc))
+}
 
 
 puts "---"
@@ -125,5 +144,6 @@ comments = doc.xpath("//*[@id='commentlisting']")
 File.open("out.html", "wb") { |file|
   file.puts("<html><body>")
   file.puts(comments.inner_html)
+  puts(comments.inner_html)
   file.puts("</body></html>")
 }
