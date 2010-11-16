7e814980e2498f2b6d33783964ee33855d414d96
Yuya Kato <yuyakato@gmail.com>
2010-11-16 13:20:01 +0900

    解析処理を整理

--- slashdotjp.rb
+++ slashdotjp.rb
@@ -55,95 +55,105 @@ def merge_query(url, params)
   return uri.to_s
 end
 
+def remove_unnecessary_elements(doc)
+  doc.xpath("//comment()").remove
+  doc.xpath("//script").remove
+  doc.xpath("//noscript").remove
+  doc.xpath("//text()").
+    select { |node| node.to_s.strip.empty? }.
+    each   { |node| node.remove }
+end
+
+def extract_information(doc)
+  details = doc.xpath("//*[@id='articles']//div[@class='details']/node()").map { |node|
+    case
+    when node.text?                         then node.text.gsub(/\s+/, " ").strip
+    when node.element? && node.name == "a"  then node.text.strip
+    when node.element? && node.name == "br" then "\n"
+    else raise("unexcepted node")
+    end
+  }.join("")
+
+  return {
+    :title           => doc.xpath("//*[@id='articles']//div[@class='title']/h3/a/text()").text.strip,
+    :published       => details.split(/\n/)[0],
+    :department      => details.split(/\n/)[1],
+    :body_element    => doc.xpath("//*[@id='articles']//div[@class='intro']"),
+    :comment_element => doc.xpath("//*[@id='commentlisting']"),
+  }
+end
+
+def remove_unnecessary_comment_elements(doc)
+  doc.xpath("//*[@id='roothiddens']").remove
+  doc.xpath("//*[@id='commentlisting']//div[@class='commentstatus']").remove
+  doc.xpath("//*[@id='commentlisting']//div[@class='commentSub']").remove
+  doc.xpath("//*[@id='commentlisting']//a[@class='user_journal_display']").each { |node| node.parent.remove }
+end
+
+def adjust_comment_title_elements(doc)
+  doc.xpath("//*[@id='commentlisting']//h4/a").each { |node|
+    text = node.text.strip
+    node.replace(Nokogiri::XML::Text.new(text, doc))
+  }
+  doc.xpath("//*[@id='commentlisting']//h4/span").each { |node|
+    text = node.text.strip
+    elem = Nokogiri::XML::Element.new("span", doc)
+    elem.add_child(Nokogiri::XML::Text.new(text, doc))
+    node.replace(elem)
+  }
+  doc.xpath("//*[@id='commentlisting']//h4").each { |node|
+    node.children.each { |child|
+      node.parent.add_child(child)
+    }
+    node.remove
+  }
+end
+
+def adjust_comment_body_elements(doc)
+  doc.xpath("//*[@id='commentlisting']//div[@class='details']//a").each { |node|
+    text = node.text.strip
+    node.replace(Nokogiri::XML::Text.new(text, doc))
+  }
+  doc.xpath("//*[@id='commentlisting']//li/b/a").each { |node|
+    text = node.text.strip
+    node.replace(Nokogiri::XML::Text.new(text, doc))
+  }
+end
+
+def remove_id_attributes(doc)
+  doc.xpath("//*").each { |node|
+    node.remove_attribute("id")
+  }
+end
+
 
 logger = create_logger
 http   = create_http_client(logger)
 
 url1 = "http://slashdot.jp/hardware/10/11/14/0416243.shtml"
 src1 = http.get(url1)
-
-p url2 = get_canonical_url(src1)
-p url3 = merge_query(url2,
+url2 = get_canonical_url(src1)
+url3 = merge_query(url2,
   "threshold"   => "1",      # 閾値: 1
   "mode"        => "nested", # ネストする
   "commentsort" => "0")      # 古い順
-
-
 src3 = http.get(url3)
 #File.open("tmp.html", "wb") { |file| file.write(src3) }
 
 doc = Nokogiri.HTML(src3)
 
-doc.xpath("//comment()").remove
-doc.xpath("//script").remove
-doc.xpath("//noscript").remove
+remove_unnecessary_elements(doc)
+remove_unnecessary_comment_elements(doc)
+adjust_comment_title_elements(doc)
+adjust_comment_body_elements(doc)
+info = extract_information(doc)
+p info
+remove_id_attributes(doc)
 
-details = doc.xpath("//*[@id='articles']//div[@class='details']/node()").map { |node|
-  case
-  when node.text?                         then node.text.gsub(/\s+/, " ").strip
-  when node.element? && node.name == "a"  then node.text.strip
-  when node.element? && node.name == "br" then "\n"
-  else raise("unexcepted node")
-  end
-}.join("")
-
-puts "---"
-p({
-  :title      => doc.xpath("//*[@id='articles']//div[@class='title']/h3/a/text()").text.strip,
-  :published  => details.split(/\n/)[0],
-  :department => details.split(/\n/)[1],
-  :body_html  => doc.xpath("//*[@id='articles']//div[@class='intro']").inner_html.strip,
-})
-
-
-puts "---"
-
-doc.xpath("//*[@id='commentlisting']//text()").
-  select { |node| node.to_s.strip.empty? }.
-  each   { |node| node.remove }
-
-doc.xpath("//*[@id='roothiddens']").remove
-doc.xpath("//*[@id='commentlisting']//div[@class='commentstatus']").remove
-doc.xpath("//*[@id='commentlisting']//div[@class='commentSub']").remove
-doc.xpath("//*[@id='commentlisting']//a[@class='user_journal_display']").each { |node| node.parent.remove }
-
-doc.xpath("//*[@id='commentlisting']//h4/a").each { |node|
-  text = node.text.strip
-  node.replace(Nokogiri::XML::Text.new(text, doc))
-}
-
-doc.xpath("//*[@id='commentlisting']//h4/span").each { |node|
-  text = node.text.strip
-  elem = Nokogiri::XML::Element.new("span", doc)
-  elem.add_child(Nokogiri::XML::Text.new(text, doc))
-  node.replace(elem)
-}
-
-doc.xpath("//*[@id='commentlisting']//h4").each { |node|
-  node.children.each { |child|
-    node.parent.add_child(child)
-  }
-  node.remove
-}
-
-doc.xpath("//*[@id='commentlisting']//div[@class='details']//a").each { |node|
-  text = node.text.strip
-  node.replace(Nokogiri::XML::Text.new(text, doc))
-}
-
-
-doc.xpath("//*[@id='commentlisting']//li/b/a").each { |node|
-  text = node.text.strip
-  node.replace(Nokogiri::XML::Text.new(text, doc))
-}
-
-
-puts "---"
-comments = doc.xpath("//*[@id='commentlisting']")
 
 File.open("out.html", "wb") { |file|
   file.puts("<html><body>")
-  file.puts(comments.inner_html)
-  puts(comments.inner_html)
+  file.puts(info[:comment_element].to_html)
+#  puts(comments.to_html)
   file.puts("</body></html>")
 }
