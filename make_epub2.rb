1d64d3d4cc22f0e8009ebe25269d3cc30e35d559
Yuya Kato <yuyakato@gmail.com>
2010-11-18 23:50:20 +0900

    写真もEPUBに格納するように変更

--- make_epub2.rb
+++ make_epub2.rb
@@ -39,11 +39,19 @@ title     = manifest["title"]     || Time.now.strftime("%Y-%m-%d %H:%M:%S")
 author    = manifest["author"]    || "Unknown"
 publisher = manifest["publisher"] || nil
 
+image_count = 0
+
 articles = manifest["urls"].each_with_index.map { |url, index|
   article = AsahiCom.new(:url => url, :http => http).parse
-  article.merge(
-    "id"       => "text#{index + 1}",
-    "filename" => "text/text#{index + 1}.xhtml")
+  article["id"]       = "text#{index + 1}"
+  article["filename"] = "text/text#{index + 1}.xhtml"
+  article["images"].each { |image|
+    image_count += 1
+    image["id"]       = "image#{image_count}"
+    image["filename"] = "images/image#{image_count}.jpg"
+    image["binary"]   = http.get(image["url"])
+  }
+  article
 }
 
 
@@ -55,29 +63,29 @@ toc_ncx_erb     = File.open("template/toc.ncx.erb",     "rb") { |file| file.read
 asahi_com_xhtml_erb = File.open("template/asahi_com.xhtml.erb", "rb") { |file| file.read }
 asahi_com_css   = File.open("template/asahi_com.css",   "rb") { |file| file.read }
 
+opf_items = [
+  {:id => "style1", :href => "styles/asahi_com.css", :type => "text/css"},
+]
+articles.each { |article|
+  opf_items << {:id => article["id"], :href => article["filename"], :type => "application/xhtml+xml"}
+  article["images"].each { |image|
+    opf_items << {:id => image["id"], :href => image["filename"], :type => "image/jpeg"}
+  }
+}
 
 env = Object.new.instance_eval {
   @uuid      = CGI.escapeHTML(uuid)
   @title     = CGI.escapeHTML(title)
   @author    = CGI.escapeHTML(author)
   @publisher = CGI.escapeHTML(publisher)
-  @items     = articles.map { |article|
-    {:id => article["id"], :href => article["filename"], :type => "application/xhtml+xml"}
-  } + [
-    {:id => "style1", :href => "styles/asahi_com.css", :type => "text/css"},
-  ]
+  @items     = opf_items
   @itemrefs  = articles.map { |article|
     {:idref => article["id"]}
   }
   binding
 }
-
 content_opf = ERB.new(content_opf_erb, nil, "-").result(env)
 
-=begin
-  <item id="image1" href="images/image1.png" media-type="image/png"/>
-  <item id="style1" href="styles/style1.css" media-type="text/css"/>
-=end
 
 env = Object.new.instance_eval {
   @uuid      = CGI.escapeHTML(uuid)
@@ -90,11 +98,13 @@ env = Object.new.instance_eval {
 }
 toc_ncx = ERB.new(toc_ncx_erb, nil, "-").result(env)
 
+
 docs = articles.map { |article|
   env = Object.new.instance_eval {
-    @title = CGI.escapeHTML(article["title"])
-    @url   = CGI.escapeHTML(article["url"])
-    @body  = article["body_html"]
+    @title  = CGI.escapeHTML(article["title"])
+    @url    = CGI.escapeHTML(article["url"])
+    @body   = article["body_html"]
+    @images = article["images"].map { |image| [CGI.escapeHTML("../" + image["filename"]), CGI.escapeHTML(image["caption"])] }
     binding
   }
   {
@@ -114,6 +124,11 @@ Zip::ZipFile.open(filename, Zip::ZipFile::CREATE) { |zip|
   zip.get_output_stream("OEBPS/toc.ncx") { |io| io.write(toc_ncx) }
   docs.each { |doc|
     zip.get_output_stream(doc["filename"]) { |io| io.write(doc["xhtml"]) }
+  }
+  articles.each { |article|
+    article["images"].each { |image|
+      zip.get_output_stream("OEBPS/" + image["filename"]) { |io| io.write(image["binary"]) }
+    }
   }
   zip.get_output_stream("OEBPS/styles/asahi_com.css") { |io| io.write(asahi_com_css) }
 }
