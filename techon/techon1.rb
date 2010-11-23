a76c4ad59651d9a07865b4241d1470b44d80cbe3
Yuya Kato <yuyakato@gmail.com>
2010-11-23 22:43:11 +0900

    イメージ、本文の解析処理を追加

--- techon/techon1.rb
+++ techon/techon1.rb
@@ -35,21 +35,58 @@ url1 = "http://techon.nikkeibp.co.jp/article/NEWS/20101111/187322/?ref=rss"
 src1 = http.get(url1)
 #puts src1
 
+def remove_unnecessary_elements(doc)
+  doc.xpath("//comment()").remove
+  doc.xpath("//script").remove
+  doc.xpath("//noscript").remove
+  doc.xpath("//text()").
+    select { |node| node.text.strip.empty? }.
+    each   { |node| node.remove }
+end
+
+def extract_images(src, url)
+  doc  = Nokogiri.HTML(src)
+  divs = doc.xpath('//*[@id="kiji"]/div[@class="bpbox_right"]/div[@class="bpimage_set"]')
+  return divs.map { |div|
+    path    = div.xpath('./div[@class="bpimage_image"]//img').first[:src]
+    url     = URI.join(url, path).to_s
+    caption = div.xpath('./div[@class="bpimage_caption"]//text()').text.strip
+    {"url" => url, "caption" => caption}
+  }
+end
+
+require "pp"
+
+
+puts "---"
+pp extract_images(src1, url1)
+
 doc = Nokogiri.HTML(src1)
+remove_unnecessary_elements(doc)
+body = doc.xpath('//*[@id="kiji"]').first
 
-body = doc.xpath('//*[@id="kiji"]')
+# 不要なdiv要素を削除
+body.xpath('./div[@class="bpbox_right"]').remove
 
-body.xpath('./div[@class="bpbox_right"]/div[@class="bpimage_set"]').each { |div|
-  puts "---"
-  #p div
-  p path = div.xpath('./div[@class="bpimage_image"]//img').first[:src]
-  p url  = URI.join(url1, path).to_s
-  p caption = div.xpath('./div[@class="bpimage_caption"]//text()').text.strip
+# 不要なid属性を削除
+body.remove_attribute("id")
+
+# 本文内のp要素をクリーンアップ
+body.xpath('.//p/text()').each { |node|
+  text = node.text.strip.sub(/^　/, "")
+  node.replace(Nokogiri::XML::Text.new(text, doc))
 }
 
+puts "---"
+# リンクをURLに置換
+body.xpath('.//a').each { |anchor|
+  path = anchor[:href]
+  url  = URI.join(url1, path).to_s
+  anchor.set_attribute("href", url)
+}
 
-=begin
+puts body.to_xml(:indent => 0, :encoding => "UTF-8")
+
 File.open("out.html", "wb") { |file|
   file.puts(body.to_html)
 }
-=end
