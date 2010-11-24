2651a7555d3f4cbd5e58fde4ce837e090bd0dc07
Yuya Kato <yuyakato@gmail.com>
2010-11-24 11:01:22 +0900

    新しいasahi.comの解析ライブラリを使うように変更

--- make_epub2.rb
+++ make_epub2.rb
@@ -11,7 +11,7 @@ require "zip/zip"
 $: << File.join(File.dirname(__FILE__), "lib")
 require "http/factory"
 require "http/message_pack_store"
-require "lib/asahi_com"
+require "asahi.com/article"
 
 def create_http_client(logger)
   store = HttpClient::MessagePackStore.new(File.join(File.dirname(__FILE__), "cache"))
@@ -42,31 +42,24 @@ publisher = manifest["publisher"] || nil
 image_count = 0
 
 articles = manifest["urls"].each_with_index.map { |url, index|
-  article = AsahiCom.new(:url => url, :http => http).parse
+  article = AsahiCom::Article.get(http, url)
   article["id"]       = "text#{index + 1}"
-  article["filename"] = "text/text#{index + 1}.xhtml"
   article["images"].each { |image|
     image_count += 1
     image["id"]       = "image#{image_count}"
-    image["filename"] = "images/image#{image_count}.jpg"
-    image["binary"]   = http.get(image["url"])
   }
   article
 }
-
 
 
 mimetype        = File.open("template/mimetype",        "rb") { |file| file.read }
 container_xml   = File.open("template/container.xml",   "rb") { |file| file.read }
 content_opf_erb = File.open("template/content.opf.erb", "rb") { |file| file.read }
 toc_ncx_erb     = File.open("template/toc.ncx.erb",     "rb") { |file| file.read }
-asahi_com_xhtml_erb = File.open("template/asahi_com.xhtml.erb", "rb") { |file| file.read }
-asahi_com_css   = File.open("template/asahi_com.css",   "rb") { |file| file.read }
 toc_xhtml_erb   = File.open("template/asahi_com_toc.xhtml.erb", "rb") { |file| file.read }
 
 opf_items = [
   {:id => "toc", :href => "toc.xhtml", :type => "application/xhtml+xml"},
-  {:id => "style1", :href => "styles/asahi_com.css", :type => "text/css"},
 ]
 articles.each { |article|
   opf_items << {:id => article["id"], :href => article["filename"], :type => "application/xhtml+xml"}
@@ -110,21 +103,6 @@ env = Object.new.instance_eval {
 toc_xhtml = ERB.new(toc_xhtml_erb, nil, "-").result(env)
 
 
-docs = articles.map { |article|
-  env = Object.new.instance_eval {
-    @url       = CGI.escapeHTML(article["url"])
-    @title     = CGI.escapeHTML(article["title"])
-    @published = CGI.escapeHTML(article["published"])
-    @body      = article["body_html"]
-    @images    = article["images"].map { |image| [CGI.escapeHTML("../" + image["filename"]), CGI.escapeHTML(image["caption"])] }
-    binding
-  }
-  {
-    "filename" => "OEBPS/" + article["filename"],
-    "xhtml"    => ERB.new(asahi_com_xhtml_erb, nil, "-").result(env),
-  }
-}
-
 filename = "epub2.epub"
 File.unlink(filename) if File.exist?(filename)
 Zip::ZipFile.open(filename, Zip::ZipFile::CREATE) { |zip|
@@ -135,13 +113,12 @@ Zip::ZipFile.open(filename, Zip::ZipFile::CREATE) { |zip|
   zip.get_output_stream("OEBPS/content.opf") { |io| io.write(content_opf) }
   zip.get_output_stream("OEBPS/toc.ncx") { |io| io.write(toc_ncx) }
   zip.get_output_stream("OEBPS/toc.xhtml") { |io| io.write(toc_xhtml) }
-  docs.each { |doc|
-    zip.get_output_stream(doc["filename"]) { |io| io.write(doc["xhtml"]) }
+  articles.each { |article|
+    zip.get_output_stream("OEBPS/" + article["filename"]) { |io| io.write(article["file"]) }
   }
   articles.each { |article|
     article["images"].each { |image|
-      zip.get_output_stream("OEBPS/" + image["filename"]) { |io| io.write(image["binary"]) }
+      zip.get_output_stream("OEBPS/" + image["filename"]) { |io| io.write(image["file"]) }
     }
   }
-  zip.get_output_stream("OEBPS/styles/asahi_com.css") { |io| io.write(asahi_com_css) }
 }
