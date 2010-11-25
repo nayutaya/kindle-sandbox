6f9997eb480da7284c36313e5540e9cc47c0175f
Yuya Kato <yuyakato@gmail.com>
2010-11-25 22:45:50 +0900

    解析処理をファイルに切り出し

--- slashdot.jp/slashdot.rb
+++ slashdot.jp/slashdot.rb
@@ -71,98 +71,7 @@ module Slashdot
   end
 end
 
-def extract_title(src)
-  doc = Nokogiri.HTML(src)
-  return doc.xpath('//*[@id="articles"]//div[@class="title"]/h3/a/text()').text.strip
-end
-
-def extract_published_time(src)
-  doc = Nokogiri.HTML(src)
-  details = doc.xpath('//*[@id="articles"]//div[@class="details"]').text.strip
-  raise unless /(\d+)年(\d+)月(\d+)日 (\d+)時(\d+)分の掲載/ =~ details
-  return Time.local($1.to_i, $2.to_i, $3.to_i, $4.to_i, $5.to_i)
-end
-
-def extract_editor(src)
-  doc = Nokogiri.HTML(src)
-  return doc.xpath('//*[@id="articles"]//div[@class="details"]/a/text()').text.strip
-end
-
-def extract_department(src)
-  doc = Nokogiri.HTML(src)
-  details = doc.xpath('//*[@id="articles"]//div[@class="details"]').text.strip
-  return details.split(/\s+/).last
-end
-
-def extract_body(src)
-  doc = Nokogiri.HTML(src)
-
-  # 全体の不要な要素を削除
-  doc.xpath("//comment()").remove
-  doc.xpath("//script").remove
-  doc.xpath("//noscript").remove
-  doc.xpath("//text()").
-    select { |node| node.text.strip.empty? }.
-    each   { |node| node.remove }
-
-  intro = doc.xpath('//*[@id="articles"]//div[@class="intro"]').first
-  intro.remove_attribute("class")
-  full = doc.xpath('//*[@id="articles"]//div[@class="full"]').first
-  full.remove_attribute("class") if full
-
-  body  = intro.to_xml(:indent => 0, :encoding => "UTF-8")
-  body += full.to_xml(:indent => 0, :encoding => "UTF-8") if full
-
-  return body
-end
-
-def extract_comments(src)
-  doc = Nokogiri.HTML(src)
-
-  # 全体の不要な要素を削除
-  doc.xpath("//comment()").remove
-  doc.xpath("//script").remove
-  doc.xpath("//noscript").remove
-  doc.xpath("//text()").
-    select { |node| node.text.strip.empty? }.
-    each   { |node| node.remove }
-
-  list = doc.xpath('//ul[@id="commentlisting"]')
-
-  return extract_child_comments(list)
-end
-
-def extract_child_comments(list)
-  # 不要なli要素を削除
-  list.xpath('./li[@class="hide"]').remove
-  # [コメントを書く]、[親コメント]などのリンクを削除
-  list.xpath('.//span[@class="nbutton"]').remove
-  # [n 個のコメント が現在のしきい値以下です。]を削除
-  list.xpath('./li/b').each { |item| item.parent.remove }
-
-  return list.xpath('./li').map { |item|
-    title        = item.xpath('./div/div/div[@class="title"]/h4/a/text()').text.strip
-    score        = item.xpath('./div/div/div[@class="title"]/h4/span//text()').text.strip.slice(1..-2)
-    user         = item.xpath('./div/div/div[@class="details"]//text()').first.text.strip
-    otherdetails = item.xpath('./div/div/div[@class="details"]/span[@class="otherdetails"]/text()').text.strip
-    body         = item.xpath('./div/div[@class="commentBody"]/div').first
-    children     = item.xpath('./ul').first
-
-    raise("invalid format") unless /(\d+)年(\d+)月(\d+)日 +(\d+)時(\d+)分/ =~ otherdetails
-    time = Time.local($1.to_i, $2.to_i, $3.to_i, $4.to_i, $5.to_i)
-    body.remove_attribute("id")
-
-    {
-      "title"    => title,
-      "score"    => score,
-      "user"     => user,
-      "time"     => time,
-      "body"     => body.to_xml(:indent => 0, :encoding => "UTF-8"),
-      "comments" => (children ? extract_child_comments(children) : []),
-    }
-  }
-end
-
+require "article_parser"
 
 
 
@@ -175,15 +84,8 @@ p commented_url = Slashdot::Article.get_commented_url(canonical_url)
 
 src = http.get(commented_url)
 
-p title          = extract_title(src)
-p published_time = extract_published_time(src)
-p editor         = extract_editor(src)
-p department     = extract_department(src)
-puts "---"
-puts body           = extract_body(src)
-puts "---"
 require "pp"
-pp comments       = extract_comments(src)
+pp Slashdot::ArticleParser.extract(src, url)
 
 File.open("out.html", "wb") { |file|
   file.write(src)
