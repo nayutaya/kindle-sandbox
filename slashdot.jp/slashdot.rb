6e0903cad0f01b3c133a60993e5b7c47dea2907c
Yuya Kato <yuyakato@gmail.com>
2010-11-25 22:09:08 +0900

    コメントの解析処理を追加

--- slashdot.jp/slashdot.rb
+++ slashdot.jp/slashdot.rb
@@ -108,18 +108,54 @@ def extract_body(src)
   intro = doc.xpath('//*[@id="articles"]//div[@class="intro"]').first
   intro.remove_attribute("class")
   full = doc.xpath('//*[@id="articles"]//div[@class="full"]').first
-  full.remove_attribute("class")
+  full.remove_attribute("class") if full
 
   body  = intro.to_xml(:indent => 0, :encoding => "UTF-8")
-  body += full.to_xml(:indent => 0, :encoding => "UTF-8")
+  body += full.to_xml(:indent => 0, :encoding => "UTF-8") if full
 
   return body
 end
 
+def extract_comment(src)
+  doc = Nokogiri.HTML(src)
+
+  # 全体の不要な要素を削除
+  doc.xpath("//comment()").remove
+  doc.xpath("//script").remove
+  doc.xpath("//noscript").remove
+  doc.xpath("//text()").
+    select { |node| node.text.strip.empty? }.
+    each   { |node| node.remove }
+
+  comments = doc.xpath('//*[@id="commentlisting"]')
+  # 不要なli要素を削除
+  comments.xpath('./li[@class="hide"]').remove
+  # [コメントを書く]、[親コメント]などのリンクを削除
+  comments.xpath('.//span[@class="nbutton"]').remove
+  # [n 個のコメント が現在のしきい値以下です。]を削除
+  comments.xpath('./li/b').each { |item| item.parent.remove }
+
+  comments.xpath('./li').each { |item|
+puts "---"
+#p item
+p title    = item.xpath('./div/div/div[@class="title"]/h4/a/text()').text.strip
+p score    = item.xpath('./div/div/div[@class="title"]/h4/span//text()').text.strip.slice(1..-2)
+p username = item.xpath('./div/div/div[@class="details"]//text()').first.text.strip
+p otherdetails = item.xpath('./div/div/div[@class="details"]/span[@class="otherdetails"]/text()').text.strip
+  raise unless /(\d+)年(\d+)月(\d+)日 (\d+)時(\d+)分/ =~ otherdetails
+p time = Time.local($1.to_i, $2.to_i, $3.to_i, $4.to_i, $5.to_i)
+puts body = item.xpath('./div/div[@class="commentBody"]/div').to_xml(:indent => 0, :encoding => "UTF-8")
+
+  }
+
+  return comments.to_xml(:indent => 0, :encoding => "UTF-8")
+end
+
+
 
 
 url = "http://slashdot.jp/it/article.pl?sid=10/09/06/2337200"
-url = "http://slashdot.jp/interview/06/01/12/0510208.shtml"
+#url = "http://slashdot.jp/interview/06/01/12/0510208.shtml"
 
 p canonical_url = Slashdot::Article.get_canonical_url(http, url)
 p commented_url = Slashdot::Article.get_commented_url(canonical_url)
@@ -130,7 +166,10 @@ p title          = extract_title(src)
 p published_time = extract_published_time(src)
 p editor         = extract_editor(src)
 p department     = extract_department(src)
+puts "---"
 puts body           = extract_body(src)
+puts "---"
+puts comment        = extract_comment(src)
 
 File.open("out.html", "wb") { |file|
   file.write(src)
